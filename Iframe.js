fa2366d8522745fc18e7a8781655ff2cc3b4b8cc
Nicholas Adamou <nicholasadamouemail@gmail.com>
2021-09-11 06:52:38 -0400

    mv Iframe to repo root

---
+++ Iframe.js
@@ -0,0 +1,34 @@
+import React, {useEffect} from "react";
+
+const Iframe = (props) => {
+	const { src } = props;
+
+	const iframe = React.createRef();
+
+	const method = 'GET';
+	const headers = new Headers({...props.headers});
+	const options = { method, headers };
+
+	const get = React.useRef(() => {});
+
+	get.current = () => {
+		fetch(src, options)
+			.then(response => response.text())
+			.then(response => {
+				const frame = iframe.current.contentWindow || ( iframe.current.contentDocument.document || iframe.current.contentDocument);
+
+				frame.document.open();
+				frame.document.write(response);
+				frame.document.close();
+			})
+			.catch(e => console.log(`react-iframe-error: ${e}`));
+	}
+
+	useEffect(() => {
+		get.current();
+	}, []);
+
+	return <iframe src={src} ref={iframe} title={"react-iframe"} {...props} />;
+}
+
+export default Iframe;
